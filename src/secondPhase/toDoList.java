6ec822aada676e8fa3c47070aa4184eb1548b269
rsomo <richard.somodsky@gmail.com>
2026-03-22 22:01:05 +0100

    starting

---
+++ src/secondPhase/toDoList.java
@@ -0,0 +1,16 @@
+package secondPhase;
+
+import java.util.Scanner;
+
+public class toDoList {
+
+    static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        String velkyCiel;
+        String malyCiel;
+        int pocetCielov;
+
+    }
+
+}
