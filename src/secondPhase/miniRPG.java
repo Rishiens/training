fb2db2ec2f0958490886b489c04e42cdbcd7f2c8
PC <richard.somodsky@gmail.com>
2026-03-24 17:34:44 +0100

    rpg

---
+++ src/secondPhase/miniRPG.java
@@ -0,0 +1,23 @@
+package secondPhase;
+
+import java.util.Arrays;
+
+public class miniRPG {
+
+     public static void main(String[] args) {
+
+         boolean spustenie = true;
+        int hpHrac = 100;
+        int utokHrac = 5;
+        int utokNepriatel = 10;
+        int hpNepriatel = 100;
+        String[] schopnosti = {"silný útok","healing", "útek"};
+
+         // tu pojdu uvodne texty
+
+         //menu hry
+         //si v hre a musis si vybrat utok z array
+
+
+    }
+}
